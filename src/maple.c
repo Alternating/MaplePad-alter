9e3489793e567881d80b00913e8eb7e3b1298a75
Alternating <tneelis@gmail.com>
2025-06-23 14:54:34 -0500

    maple.c buttons updated

--- src/maple.c
+++ src/maple.c
@@ -87,8 +87,8 @@ ButtonInfo ButtonInfos[NUM_BUTTONS] = {
     {10, 0x0008} // Start
 #if HKT7300
     ,
-    {16, 0x0001}, // C
-    {17, 0x0100}  // Z
+    {24, 0x0001}, // C - UPDATED from GP16 to GP24
+    {25, 0x0100}  // Z - UPDATED from GP17 to GP25
 #endif
 };
 
